6648a3642a5c098ec912306f63c1b67ab6beca6b
Fernandez Guillermo <fernandezguillermo86@gmail.com>
2023-05-15 17:46:36 -0300

    mods in database

--- db/database.sql
+++ db/database.sql
@@ -3,9 +3,9 @@ CREATE DATABASE companydb;
 
 USE companydb;
 
-CREATE TABLE employee{
+CREATE TABLE employee(
     id INT(11) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(45) DEFAUL NULL,
+    name VARCHAR(45) DEFAULT NULL,
     salary INT(5) DEFAULT NULL,
     PRIMARY KEY (id)
-}
+)
